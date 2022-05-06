22c4ae860f95e13824488f2d809b7a6b149ef374
paulinnha <paulafaverofarias17@hotmail.com>
2022-05-06 09:00:05 -0300

    progr1.0.0

--- conexão.js
+++ conexão.js
@@ -8,9 +8,10 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err) {
+  if (err) throw err;
+  var sql = "SELECT users.name AS user, products.name AS favorite FROM users JOIN products ON users.favorite_product = products.id";
+  con.query(sql, function (err, result) {
     if (err) throw err;
-    con.query("SELECT * FROM customers WHERE address LIKE 'S%'", function (err, result) {
-      if (err) throw err;
-      console.log(result);
-    });
+    console.log(result);
   });
+});
